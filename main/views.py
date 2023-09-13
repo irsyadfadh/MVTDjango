ed85ba4e293ef969ba2f72e7d41e3424f09b209d
irsyadfadh <irsyad.fadhilah@ui.ac.id>
2023-09-13 11:28:30 +0700

    kreasi HTML

--- main/views.py
+++ main/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 
 def show_main(request):
     context = {
+        'deskripsi': 'Berikut merupakan data pribadi mahasiswa kelas PBP semester gasal :',
         'name': 'Irsyad Fadhilah',
-        'class': 'PBP B'
+        'class': 'PBP B',
+        'npm': '2206083363',
+        'jurusan': 'Sistem Informasi',
+        'angkatan': 'apollo 2022',
     }
 
     return render(request, "main.html", context)
